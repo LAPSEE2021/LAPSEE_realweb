cfe7153f02f24b2a249f3c118f64568c0ea5f5dd
KHsuan <50944459+KHsuan@users.noreply.github.com>
2022-02-01 23:24:29 +0800

    brandnew

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,30 @@
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
 
+    var textWrapper = document.querySelector('.ml2');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: false})
+    .add({
+        targets: '.ml2 .letter',
+        scale: [4,1],
+        opacity: [0,1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: (el, i) => 70*i
+    }).add({
+        targets: '.ml2',
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+    
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
@@ -50,31 +73,9 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
-    var textWrapper = document.querySelector('.ml2');
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-    anime.timeline({loop: false})
-    .add({
-        targets: '.ml2 .letter',
-        scale: [4,1],
-        opacity: [0,1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 950,
-        delay: (el, i) => 70*i
-    }).add({
-        targets: '.ml2',
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-    });
-
+    
 });
+
+
 
 
